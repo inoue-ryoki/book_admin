63ea1ea1debd4a545038ed9ac75962093bbad765
inoue-ryoki <mth.1021@icloud.com>
2023-11-01 19:25:54 +0900

    完成

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,12 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+  end
+
+  def high_price?
+    price >= 5000
+  end
 end
